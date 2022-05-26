87faeb584a3e76def4ac593b2ca35b3cb2365c5e
Kuldeep-Chotrani <kuldeepramani17@gmail.com>
2022-05-26 11:32:30 +0530

    Camhire_First_Push

---
+++ src/components/photographers.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Photographers_card } from "./photographers-card"
+import { Photographers_card_2 } from "./photographers-card-2"
+import Pagination from "./Pagination";
+import Card from "./Card";
+import '../scss_files/photographer.scss'
+import Collapsible from './collapsible.jsx';
+export const Photographers = (props) => {
+  const [showing, setIsShowing] = useState(true);
+  const [photographerIndex, setPhotographerIndex] = useState(-1);
+  const [productDataList, setProductDataList] = useState([]);
+
+ const resetStatus = () => {
+   setIsShowing(true);
+   setPhotographerIndex(-1)
+ }
+ const expandPhotographer = (index) => {
+  setIsShowing(false);
+  setPhotographerIndex(index);
+  console.log("called")
+ }
+ const onChangePage = (pageOfItems) => {
+    setProductDataList(pageOfItems)
+};
+  return (
+    <>
+    {showing && photographerIndex ===-1?
+    <div id='photographers' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2 data-aos="fade-up" data-aos-duration="1000">Our Photographers</h2>
+          <p data-aos="fade-up" data-aos-duration="1000">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+        <div className="portfolio-item cursor">
+          {/* <Photographers_card_2 /> */}
+          {/* <div className="col-md-4"><Photographers_card /></div>
+              <div className="col-md-4"><Photographers_card /></div>
+              <div className="col-md-4"><Photographers_card /></div>
+              <div className="col-md-4"><Photographers_card /></div>
+              <div className="col-md-4"><Photographers_card /></div>
+              <div className="col-md-4"><Photographers_card /></div> */}
+         
+          {productDataList
+            ? productDataList.map((d, i) => (
+          <div key={`${d.title}-${i}`} onClick = {(i) => expandPhotographer(d.index)} className="col-md-4" data-aos='fade-up' data-aos-duration='1000'>
+            <Card
+              title={d.title}
+              imageUrl={d.imageUrl}
+              body={d.body}
+              setIsPhotographerPosition ={setPhotographerIndex} 
+              index = {i}
+              setIsShowData = {setIsShowing}
+              
+              />
+          </div>
+))
+: 'loading'}</div>
+        </div>
+      </div>
+      <Pagination
+          pageSize={3}
+          items={props.data}
+          onChangePage={onChangePage}
+        />
+    </div>:<>
+
+       <div id='photographers' className='text-center'>
+       <a href="#photographers" onClick={resetStatus}>back</a>
+       <div className='container'>
+       {props.data && photographerIndex !==-1
+            ?
+            <>
+         <div className='section-title'>
+           <h2 data-aos="fade-up" data-aos-duration="1000">{props.data[photographerIndex].title}</h2>
+           <p data-aos="fade-up" data-aos-duration="1000">
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+             dapibus leonec.
+           </p>
+         </div>
+          <div key={photographerIndex} className="col-md-4" data-aos='fade-up' data-aos-duration='1000'>
+          <div className='container'>
+        <div className='row'>
+          <div className='about-img-cam col-xs-12 col-md-6' data-aos="fade" data-aos-duration="1500">
+            {' '}
+            <img src={props.data[photographerIndex].imageUrl} className='img-responsive' alt='' />{' '}
+          </div>
+          <div className='about-text-title col-xs-12 col-md-6'>
+            <div className='about-text'>
+              <h2 data-aos="fade-up" data-aos-duration="1500">{props.data[photographerIndex].codeword}</h2 >
+              <p data-aos="fade-up" data-aos-duration="1000">{props.data[photographerIndex].mainbody} </p>
+              <h3 data-aos="fade-up" data-aos-duration="1000" >Speciality</h3>
+
+            </div>
+          </div>
+        </div>
+      </div>
+          </div>
+          </>:"loading"}
+         </div>
+         </div></>}
+    </>
+  )
+}
